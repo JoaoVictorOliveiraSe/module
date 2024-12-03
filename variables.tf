023b3562431082d937e2b477ab6d965868e6e7a9
JoaoVictorOliveiraSe <joao_se@yahoo.com.br>
2024-12-03 17:13:18 -0300

    subindo terraform

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "nome" {
+  type        = string
+  description = "Nome da instancia"
+}
+
+variable "environment" {
+  type        = string
+  description = "Ambiente da instancia"
+  default     = "dev"
+}
+
+variable "ami" {
+  type = string
+  description = "AMI da instancia"
+}
